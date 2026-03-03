96ac2d0b504401b5c7314751c5795564414a2f46
OutsydBaby <amichdot@gmail.com>
2026-03-03 16:54:44 +0100

    refactor(app): remove dotenv configuration from app setup

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,8 @@
 import express from 'express';
-import dotenv from 'dotenv';
-
-dotenv.config()
+import authRoute from './Modules/authRoutes'
 
 const app = express()
 app.use(express.json());
+app.use('/auth', authRoute)
 
 export default app;
