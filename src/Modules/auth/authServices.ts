cce7e74bb87206a71b83b90bb7a98183e2a43a4f
OutsydBaby <amichdot@gmail.com>
2026-03-03 17:27:31 +0100

    refactor: Moved authServices into auth folder

---
+++ src/Modules/auth/authServices.ts
@@ -0,0 +1,64 @@
+import bcrypt, { hash } from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { pool } from '../../Config/db';
+
+const JWT_SECRET = process.env.JWT_SECRET as string;
+
+export class AuthService{
+    // Register User
+    static async register(email: string, password: string){
+        // Check if user already exist
+        const existingUser = await pool.query(
+            'SELECT * FROM users WHERE email = $1',[email]
+        )
+        if(existingUser.rows.length > 0) {
+            throw new Error("User already exist")
+        }
+
+        // Hash Password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Save user to database
+        const result = await pool.query(
+            'INSERT INTO users (email, password_hash) VALUES ($1, $2) RETURNING id, email',[email, hashedPassword]
+        )
+
+        const user = result.rows[0]
+
+        // Create JWT
+        const token = jwt.sign(
+            {userId: user.id}, 
+            JWT_SECRET, 
+            {expiresIn: '1d'}
+        )
+        return {user, token};
+    }
+    
+    static async login(email: string, password: string){
+        // Find user by email
+        const result = await pool.query(
+            'SELECT * FROM users WHERE email = $1', [email]
+        )
+        if(result.rows.length === 0){
+            throw new Error('Invalid Credentials')
+        }
+        const user = result.rows[0];
+
+        // Compare Password
+        const checkpwd = await bcrypt.compare(password, user.password_hash)
+        if(!checkpwd){
+            throw new Error('Invalid Credentials')
+        }
+
+        // Create JWT
+        const token = jwt.sign(
+            {userId: user.id},
+            JWT_SECRET,
+            {expiresIn: '1d'}
+        )
+        return {
+            user:{id: user.id, email: user.email}, 
+            token
+        }
+    }
+}
