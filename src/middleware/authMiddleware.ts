c947974c260dad3b6845e9a859a903986d832ba6
OutsydBaby <amichdot@gmail.com>
2026-03-03 17:25:18 +0100

    feat: Added AuthMiddleware for security

---
+++ src/middleware/authMiddleware.ts
@@ -0,0 +1,36 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from 'jsonwebtoken'
+
+const JWT_SECRET = process.env.JWT_SECRET as string;
+
+export interface AuthRequest extends Request{
+    userId?: number;
+}
+
+export const protect = (req: AuthRequest, res: Response, next: NextFunction) => {
+    try {
+        // Check for header
+        const authHeader = req.headers.authorization;
+        if(!authHeader){
+            return res.status(404).json({message: 'No token provided'})
+        }
+
+        // Check Bearer token
+        const token = authHeader.split(" ")[1];
+        if(!token){
+            return res.status(401).json({message: 'Invalid Token'})
+        }
+
+        const decoded = jwt.verify(token, JWT_SECRET) as {
+            userId: number
+        }
+        // Attach UserId to request
+        req.userId = decoded.userId
+        next()
+    } catch (error) {
+        res.status(401).json({
+            status: 'fail',
+            message: 'Invalid or Expired Token', error
+        })
+    }
+}
