c14623c4a557659a3b0c1afa717638ee2940c19b
OutsydBaby <amichdot@gmail.com>
2026-03-03 17:26:17 +0100

    moved

--- src/Modules/authRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { AuthController } from "./authController";
-
-const router = Router();
-router
-    .post('/register', AuthController.register)
-    .post('/login', AuthController.login)
-
-export default router;
