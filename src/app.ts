6f946ab786322c1a89789a5c389f30b79aeb251a
OutsydBaby <amichdot@gmail.com>
2026-03-04 13:11:16 +0100

    added websiteRoutes

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import authRoute from './Modules/auth/authRoutes'
+import websiteRoutes from './Modules/website/websiteRoutes'
 
 const app = express()
 app.use(express.json());
 app.use('/auth', authRoute)
+app.use('/website', websiteRoutes)
 
 export default app;
