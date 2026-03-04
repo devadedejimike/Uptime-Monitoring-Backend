0d14cbdd4b1b4fc956e4262b660727db4934a1f0
OutsydBaby <amichdot@gmail.com>
2026-03-04 12:35:01 +0100

    feat(website): add websiteController to handle creating website links

---
+++ src/Modules/website/websiteController.ts
@@ -0,0 +1,21 @@
+import { Response } from "express";
+import { AuthRequest } from "../../Middleware/authMiddleware";
+import { WebsiteServices } from "./websiteServices";
+
+export class WebsiteController{
+    static async create(req: AuthRequest, res: Response){
+        try {
+            const { url } = req.body;
+            if(!url) {
+                return res.status(401).json({message: "URL is required"})
+            }
+            const website = await WebsiteServices.createWebsite(req.userId!, url)
+            res.status(201).json(website)
+        } catch (error) {
+            res.status(401).json({
+                status: 'fail',
+                message: 'Server Error', error
+            })
+        }
+    }
+}
