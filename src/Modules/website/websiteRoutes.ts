99c41f20e8724b268450d26f8a3edbf4fc67189b
OutsydBaby <amichdot@gmail.com>
2026-03-05 15:35:55 +0100

    feat(website): added getWebsiteStats route

--- src/Modules/website/websiteRoutes.ts
+++ src/Modules/website/websiteRoutes.ts
@@ -6,6 +6,7 @@ const router = Router();
 router
     .post('/', protect, WebsiteController.create)
     .get('/', protect, WebsiteController.getAll)
+    .get('/:id/stats', protect, WebsiteController.getWebsiteStats)
     .delete('/:id', protect, WebsiteController.deleteWebsite)
 
 export default router;
