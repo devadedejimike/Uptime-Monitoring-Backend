7b68093ab1f82f7984eb4ec540813924b9491710
OutsydBaby <amichdot@gmail.com>
2026-03-17 13:58:51 +0100

    feat: added cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from "cors";
 import authRoute from './Modules/auth/authRoutes'
 import websiteRoutes from './Modules/website/websiteRoutes'
 
 const app = express()
+app.use(cors())
 app.use(express.json());
 app.use('/auth', authRoute)
 app.use('/website', websiteRoutes)
