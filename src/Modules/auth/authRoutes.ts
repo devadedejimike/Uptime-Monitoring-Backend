e3a2a0c8ff82891285a6c0cd41130f718dc025a8
OutsydBaby <amichdot@gmail.com>
2026-03-17 14:04:48 +0100

    feat(auth): implemented route to fetch logged in user

--- src/Modules/auth/authRoutes.ts
+++ src/Modules/auth/authRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { AuthController } from "./authController";
+import { protect } from "../../Middleware/authMiddleware";
 
 const router = Router();
 router
     .post('/register', AuthController.register)
     .post('/login', AuthController.login)
+    .get('/me',protect, AuthController.getMe)
 
 export default router;
