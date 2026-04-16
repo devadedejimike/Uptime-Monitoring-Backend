a4392fe4f59182e5687677cde8c8b5e388c71c96
OutsydBaby <amichdot@gmail.com>
2026-04-16 17:41:25 +0100

    feat(worker): integrate alert service into monitoring worker and added last_status column to websites for state tracking

--- src/Modules/workers/monitorWorkers.ts
+++ src/Modules/workers/monitorWorkers.ts
@@ -1,6 +1,9 @@
 import cron from "node-cron";
 import axios from "axios";
 import { pool } from "../../Config/db";
+import { handleStatusChange } from "../alerts/alertServices";
+
+type Status = 'UP' | 'DOWN';
 
 export const startMonitoring = () => {
     // Run every 5 minutes
@@ -13,20 +16,29 @@ export const startMonitoring = () => {
             for(const website of websites.rows){
                 const start = Date.now();
 
+                let currentStatus: Status = 'UP';
                 try {
                     await axios.get(website.url, {timeout: 10000});
-                    const responseTime = Date.now() - start;
+                } catch (error){
+                    currentStatus = 'DOWN';
+                }    
+                const responseTime = Date.now() - start;
 
-                    await pool.query(
-                        "INSERT INTO checks (website_id, status, response_time_ms) VALUES ($1, $2, $3)", [website.id, "UP", responseTime]
-                    )
-                } catch (error) {
-                    const responseTime = Date.now() - start;
+                await pool.query(
+                    "INSERT INTO checks (website_id, status, response_time_ms) VALUES ($1, $2, $3)", [website.id, currentStatus, responseTime]
+                )
 
-                    await pool.query(
-                        "INSERT INTO checks (website_id, status, response_time_ms) VALUES ($1, $2, $3)",[website.id, "DOWN", responseTime]
-                    )
-                }
+                await handleStatusChange({
+                    userId: website.user_id,
+                    websiteId: website.id,
+                    websiteUrl: website.url,
+                    prevStatus: website.last_status,
+                    currentStatus,
+                })
+
+                await pool.query(
+                    "UPDATE websites SET last_status = $1 WHERE id = $2", [currentStatus, website.id]
+                );
             }
         } catch (error) {
             console.error("Cron worker error", error)
