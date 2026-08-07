8b0fd4a4620fd9f7c2a4b02c292a84466458805f
OutsydBaby <amichdot@gmail.com>
2026-08-07 18:27:27 +0100

    Refactor alert service to handle incident events

--- src/Modules/alerts/alertServices.ts
+++ src/Modules/alerts/alertServices.ts
@@ -1,42 +1,24 @@
 import { pool } from '../../Config/db';
+import { HandleIncidentEventProps } from '../../Monitor/types';
 import { sendEmail } from '../../utils/mailer';
 
-type Status = 'UP' | 'DOWN';
-
-interface handleStatusChangeProps{
-    userId: number;
-    websiteId: number,
-    websiteUrl: string,
-    prevStatus: Status | null,
-    currentStatus: Status
-};
-
-export const handleStatusChange = async ({
+// save the alert and notify the user
+export const handleIncidentEvent = async ({
     userId,
     websiteId,
     websiteUrl,
-    prevStatus,
-    currentStatus
-}: handleStatusChangeProps) => {
+    event,
+}: HandleIncidentEventProps) => {
+    // Build the alert message
+    const message = event === "DOWN" ? `${websiteUrl} is DOWN` : `${websiteUrl} is back online`
 
-    if (prevStatus === currentStatus) return;
+    const subject = event === "DOWN" ? "Website Down" : "Website Recovered";
 
-    let type: 'DOWN' | 'RECOVERY' | null = null;
-    let message = '';
-
-    if(prevStatus === 'UP' && currentStatus === 'DOWN'){
-        type = 'DOWN';
-        message = `${websiteUrl} is DOWN`
-    }
-
-    if (prevStatus === 'DOWN' && currentStatus === 'UP') {
-        type = 'RECOVERY'
-        message = `${websiteUrl} is back online`
-    }
+    // save the alert
     await pool.query(
-        `INSERT INTO alerts(user_id, website_id, type, message) VALUES ($1, $2, $3, $4)`, [userId, websiteId, type, message ]
+        `INSERT INTO alerts(user_id, website_id, type, message) VALUES ($1, $2, $3, $4)`, [userId, websiteId, event, message ]
     );    
-
+    // get the user's email address
     const userResult = await pool.query(
         `SELECT email FROM users WHERE id = $1`, [userId]
     );
@@ -46,7 +28,7 @@ export const handleStatusChange = async ({
 
     await sendEmail(
         userEmail,
-        type === 'DOWN' ? "Website Down" : "Website is Back Up",
+        subject,
         message
     )
 }
